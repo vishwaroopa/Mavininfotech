cd826f5c5fd463234fb6ad7a935e43f07b835f85
vishwaroopa <vishwarupa.vishu@gmail.com>
2024-04-06 18:21:58 +0530

    Initial commit

---
+++ components/HomePageDemo2/AboutOurCompany5.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Link from "next/link";
+
+const AboutOurCompany = () => {
+  return (
+    <>
+      <div className="about-area ptb-100">
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            
+
+            <div
+              className="col-lg-12 col-md-12"
+              
+            >
+              <div className="about-content">
+                <h2>ENTERPRISE RESOURCE PLANNING</h2>
+
+                <p><br/>
+Mavin ERP delivers the proven, comprehensive financial management capabilities required to grow a changing, complex business for small and medium businesses. Mavin ERP takes your business beyond traditional accounting software by streamlining operations across your entire organization and providing you with the real-time visibility you need to make better, faster decisions.
+
+
+<br/><br/>
+<img src="/images/mavinerp.png" /> 
+<br/><br/><br/>
+<h2>Key Benefits</h2>
+Accelerate the order-to cash process by 50%+ Slash financial close by over 50% Drive better, faster decision-making with real-time data and reporting, and personalized dashboards Improve workforce productivity with anytime, anywhere access Lower cost of ownership by eliminating upfront and ongoing IT expenditures, and delivering automatic product upgrades
+<br/><br/>  
+<h2>Articles</h2>
+</p>
+<ul><li>What is ERP?</li><li>ERP Implementation Best Practices</li><li>On-premise vs Cloud</li><li>Is Your Business Ready for an ERP System?</li></ul>               
+              </div>
+
+<p>For further information please <a href="/content/contact-us">contact us</a>.</p>
+            </div>
+          
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutOurCompany;
